0084a8c3a8d5aee7bf52e78d83f3676dac836f9a
Jakub Czajka <CzajkaPL@o2.pl>
2022-05-28 20:12:22 +0200

    Added dynamic rows to table (raw data to improve)

--- app/src/main/java/com/example/boardgamecollector/GamesExtensionsTable.kt
+++ app/src/main/java/com/example/boardgamecollector/GamesExtensionsTable.kt
@@ -1,17 +1,21 @@
 package com.example.boardgamecollector
 
+import android.graphics.Color
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_games_extensions_table.*
+import kotlin.collections.ArrayList
 
 
 class GamesExtensionsTable : AppCompatActivity() {
 
     private lateinit var database: DatabaseHelper
+    private lateinit var tableData: TableLayout
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,24 +29,47 @@ class GamesExtensionsTable : AppCompatActivity() {
     private fun addListeners() {
         showGamesButton.setOnClickListener {
             val data = database.getDataGamesExtensions(QueriesTypes.GET_GAMES_BY_NAME_ASC)
-            loadData(data)
+            createTable(data)
         }
 
         showExtensionsButton.setOnClickListener {
             val data = database.getDataGamesExtensions(QueriesTypes.GET_EXTENSIONS_BY_NAME_ASC)
-            loadData(data)
+            createTable(data)
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
-    private fun loadData(data: ArrayList<GameInfo>) {
+    private fun createTable(data: ArrayList<GameInfo>) {
+        tableData = findViewById(R.id.dataTable)
+        tableData.removeAllViews()
+        val headers = listOf("id", "Game Title", "Release Date", "Current rank", "Extension", "Img")
+        val textViews = listOf(TextView(this), TextView(this), TextView(this), TextView(this), TextView(this), TextView(this))
+        val tableRow = TableRow(this)
+        for ((index, value) in textViews.withIndex()) {
+            value.setTextColor(Color.RED)
+            value.text = headers[index]
+            tableRow.addView(value)
+        }
+        tableData.addView(tableRow)
+
         data.stream().forEach {
-            println(it)
-            val tableRow = TableRow(this)
-            val textView = TextView(this)
-            textView.text = it.toString()
-            tableRow.addView(textView)
-            dataTable.addView(tableRow)
+            val valueHeaders =
+                listOf(it.id, it.gameName, it.yearPublished, it.currentRank, it.extension, it.image)
+            val valueTextViews = listOf(
+                TextView(this),
+                TextView(this),
+                TextView(this),
+                TextView(this),
+                TextView(this),
+                TextView(this)
+            )
+            val valueTableRow = TableRow(this)
+            for ((index, value) in valueTextViews.withIndex()) {
+                value.setTextColor(Color.BLUE)
+                value.text = valueHeaders[index].toString()
+                valueTableRow.addView(value)
+            }
+            tableData.addView(valueTableRow)
         }
     }
 }
